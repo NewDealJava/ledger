fa1323ae27e5466c8bae6e2882c9d78d1f1977fc
juwoong kim <wwong7247@gmail.com>
2024-07-16 00:41:26 +0900

    add: 카드 생성 API 추가

--- src/main/java/com/newdeal/ledger/cardaccount/controller/CardRestController.java
+++ src/main/java/com/newdeal/ledger/cardaccount/controller/CardRestController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newdeal.ledger.cardaccount.dto.CardDto;
+import com.newdeal.ledger.cardaccount.dto.CardRequest;
 import com.newdeal.ledger.cardaccount.service.CardService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,4 +29,13 @@ public class CardRestController {
 
 		return cardService.selectAllByEmail(tempEmail);
 	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createCard (@ModelAttribute CardRequest.Create request) {
+		String tempEmail = "user1@example.com";
+
+		cardService.createCard(tempEmail, request);
+	}
+
 }
--- src/main/java/com/newdeal/ledger/cardaccount/service/CardService.java
+++ src/main/java/com/newdeal/ledger/cardaccount/service/CardService.java
@@ -3,7 +3,10 @@ package com.newdeal.ledger.cardaccount.service;
 import java.util.List;
 
 import com.newdeal.ledger.cardaccount.dto.CardDto;
+import com.newdeal.ledger.cardaccount.dto.CardRequest;
 
 public interface CardService {
 	List<CardDto> selectAllByEmail(String email);
+
+	void createCard(String tempEmail, CardRequest.Create request);
 }

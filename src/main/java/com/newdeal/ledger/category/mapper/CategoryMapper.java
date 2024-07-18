91ea60055d82ec9d728159e489533b0ac2687f55
juwoong kim <wwong7247@gmail.com>
2024-07-18 11:08:53 +0900

    fix: 트랜젝션 타입별 카테고리 조회 오류 수정

--- src/main/java/com/newdeal/ledger/category/mapper/CategoryMapper.java
+++ src/main/java/com/newdeal/ledger/category/mapper/CategoryMapper.java
@@ -3,8 +3,8 @@ package com.newdeal.ledger.category.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-import com.newdeal.ledger.category.dto.CategoryDto;
 import com.newdeal.ledger.category.dto.CategoryResponse;
 import com.newdeal.ledger.transaction.dto.type.TransactionType;
 
@@ -12,7 +12,7 @@ import com.newdeal.ledger.transaction.dto.type.TransactionType;
 public interface CategoryMapper {
 	List<CategoryResponse.GetOne> findCategories();
 
-	List<CategoryResponse.GetOne> findCategoriesByType(TransactionType type);
+	List<CategoryResponse.GetOne> findCategoriesByType(@Param("type") TransactionType type);
 
 	List<CategoryResponse.GetOne> findSubcategoriesByCategoryId(Integer parentCategoryId);
 }

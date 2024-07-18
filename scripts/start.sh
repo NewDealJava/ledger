529b9382513c85f7b6f4999783c1e47387a6ea0c
juwoong kim <wwong7247@gmail.com>
2024-07-18 19:33:29 +0900

    fix: start.sh 빌드명 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 # start.sh
 
 PROJECT_ROOT="/home/ubuntu/deploy"
-JAR_FILE="$PROJECT_ROOT/ledger-0.0.1-SNAPSHOT.jar"
+JAR_FILE="/home/ubuntu/app/server-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -11,7 +11,7 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/ledger-0.0.1-SNAPSHOT.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
